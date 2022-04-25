a7e7a6973482836eb4ea7b5dc2710d7cda529bb0
Gher <2504381138@qq.com>
2022-04-25 19:01:37 +0800

    modify app

--- app.py
+++ app.py
@@ -66,49 +66,61 @@ def getrules():
 
 @app.route('/class/<name>/<rule>')
 def group(name, rule):
+    if not os.path.lexists('sub'):
+        os.mkdir('sub')
+    name = escape(name)
+    rule = escape(rule)
+    if not os.path.lexists(f'sub/{name}'):
+        os.mkdir(f'sub/{name}')
     try:
-        if not os.path.lexists('sub'):
-            os.mkdir('sub')
-        name = escape(name)
-        rule = escape(rule)
-        if not os.path.lexists(f'sub/{name}'):
-            os.mkdir(f'sub/{name}')
-        try:
-            file_time = int(os.stat(f'sub/{name}/{rule}.yaml').st_mtime)
-        except:
-            file_time = 0
-        new_time = int(time.time())
-        if (new_time - file_time) >= timeout:
-            sub = sub_config.get(name)
-            urls = sub.get('url').split('|')
-            include = sub.get('include')
-            exclude = sub.get('exclude')
-            host = sub.get('host')
-            udp = sub.get('udp')
-            group = sub.get('list')
-            print(f'include: {include}\nexclude: {exclude}\nhost: {host}\nudp: {udp}')
-            LNodes = []
-            for i in urls:
-                res = requests.get(i, headers={
-                    "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KH"
-                    + "TML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.56"
-                })
-                LNodes += node(res.text, host, udp, include, exclude)
-            for k,v in group.items():
-                with open(f'sub/{name}/{k}.yaml', 'w', encoding='utf8') as f:
-                    f.write('proxies:\n')
-                    for i in LNodes:
-                        if re.search(v, i):
-                            f.write('\n'.join(['  '+j for j in i.split('\n')]))
-                            f.write('\n')
-            f = open(f'sub/{name}/{rule}.yaml', encoding='utf8')
-            data = f.read()
-            f.close()
-            return data
-        else:
-            f = open(f'sub/{name}/{rule}.yaml', encoding='utf8')
-            data = f.read()
-            f.close()
-            return data
+        file_time = int(os.stat(f'sub/{name}/{rule}.yaml').st_mtime)
     except:
-        return 'error'
+        file_time = 0
+    new_time = int(time.time())
+    if (new_time - file_time) >= timeout:
+        sub = sub_config.get(name)
+        urls = sub.get('url').split('|')
+        include = sub.get('include')
+        exclude = sub.get('exclude')
+        host = sub.get('host')
+        udp = sub.get('udp')
+        group = sub.get('list')
+        print(f'include: {include}\nexclude: {exclude}\nhost: {host}\nudp: {udp}')
+        LNodes = []
+        for i in urls:
+            res = requests.get(i, headers={
+                "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KH"
+                + "TML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.56"
+            })
+            LNodes += node(res.text, host, udp, include, exclude)
+        servers = []
+        with open(f'sub/{name}/all.yaml', 'w', encoding='utf8') as f:
+            f.write('proxies:\n')
+            f.write(re.sub('^', '  ', '\n'.join(LNodes)))
+            f.write('\n')
+        for k,v in group.items():
+            with open(f'sub/{name}/{k}.yaml', 'w', encoding='utf8') as f:
+                f.write('proxies:\n')
+                for i in LNodes:
+                    servers.append(re.search('server..(.*)', i).group(1))
+                    if re.search(v, i):
+                        f.write('\n'.join(['  '+j for j in i.split('\n')]))
+                        f.write('\n')
+        servers = list(set(servers))
+        with open(f'sub/{name}/servers.yaml', 'w', encoding='utf8') as f:
+            f.write('payload:')
+            for i in servers:
+                if re.match('\d+.\d+.\d+.\d+', i):
+                    f.write(f'\n  - IP-CIDR,{i}/32')
+                else:
+                    f.write(f'\n  - DOMAIN,{i}')
+            f.write('\n')
+        f = open(f'sub/{name}/{rule}.yaml', encoding='utf8')
+        data = f.read()
+        f.close()
+        return data
+    else:
+        f = open(f'sub/{name}/{rule}.yaml', encoding='utf8')
+        data = f.read()
+        f.close()
+        return data
